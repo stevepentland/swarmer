12d71ebda5e38eaf5595e7811e2316f2001166a3
Steve Pentland <stevepentland@gmail.com>
2018-11-05 09:31:17 -0500

    Remove WS code, add Docker elements.
    It seems that using a websocket will not provide any real benefit
    over standard HTTP POST ops.
    
    Begin adding docker code and ignore file to simplify building

--- swarmer.py
+++ swarmer.py
@@ -1,9 +1,9 @@
 from sanic import Sanic
 from sanic.response import json, text
-from ws.ws_blueprint import bp as ws_bp
+from results.result_bp import bp as res_bp
 
 app = Sanic()
-app.blueprint(ws_bp)
+app.blueprint(res_bp)
 
 
 @app.post('/submit')
@@ -23,6 +23,3 @@ async def get_job_status(request, identifier):
     :param identifier: The job identifier
     """
     pass
-
-
-app.run(host='0.0.0.0', port=8500, debug=True)
