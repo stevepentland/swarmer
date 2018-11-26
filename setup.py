ca1b07a4d8e16a3b5099a2a178f7e3b9f7cf96dd
Steve Pentland <stevepentland@users.noreply.github.com>
2018-11-26 09:22:31 -0500

    Update patch

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requires = [
 
 setuptools.setup(
     name='swarmer',
-    version='0.4.0',
+    version='0.4.2',
     author='Steve Pentland',
     author_email='swarmerproject@outlook.com',
     license='MIT',
