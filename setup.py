b268ad86dccd7e440d6bb4adf6580d0a37eb6b99
Steve Pentland <stevepentland@gmail.com>
2018-12-07 22:42:16 -0500

    Update version to pre1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requires = [
 
 setuptools.setup(
     name='swarmer',
-    version='0.5.0',
+    version='0.6.0-pre1',
     author='Steve Pentland',
     author_email='swarmerproject@outlook.com',
     license='MIT',
